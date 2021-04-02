9daff19cf79f36b3dcde40d785ae4866d83721c1
Pedto Torres <ptorres29@ucmerced.edu>
2021-04-01 19:36:55 -0700

    scaffold

--- lib/sensor.dart
+++ lib/sensor.dart
@@ -5,6 +5,8 @@ class ourSensor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        //put code here
+        );
   }
 }
