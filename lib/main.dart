0db6818c47493c029e75eeaca222f332bc818bff
Ankit Sompura <asompura@ucmerced.edu>
2021-04-01 19:56:42 -0700

    Implementing example code given

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:ppgresearch/sensor.dart';
+import 'sensor.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() {
-  runApp(MyApp());
+  final AppState state = AppState();
+
+  final MaterialApp app = MaterialApp(
+    title: 'PPG Example',
+    debugShowCheckedModeBanner: false,
+    home: ScopedModel<AppState>(model: state, child: PPGExample()),
+  );
+
+  runApp(app);
+
+  state.initPPG();
+
+  handlePermissionResponse(Map<PermissionGroup, PermissionStatus> status) {
+    if (status[PermissionGroup.sensors] == PermissionStatus.granted) {
+      state.initHR();
+    }
+  }
+
+  handlePermissionStatus(PermissionStatus status) => status ==
+          PermissionStatus.granted
+      ? state.initHR()
+      : PermissionHandler()
+          .requestPermissions(<PermissionGroup>[PermissionGroup.sensors]).then(
+              handlePermissionResponse);
+
+  PermissionHandler()
+      .checkPermissionStatus(PermissionGroup.sensors)
+      .then(handlePermissionStatus);
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class PPGExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    final AppState state = ScopedModel.of<AppState>(
+      context,
+      rebuildOnChange: true,
+    );
+
+    final String ppgTxt = !state.ppgDetected
+        ? 'PPG not detected'
+        : 'Channel 1: ${state.ppgData[0]}\n'
+            'Channel 2: ${state.ppgData[1]}\n'
+            'timestamp: ${state.ppgTimestamp.round()}\n'
+            'accuracy: ${state.ppgAccuracy}';
+
+    final String hrTxt = !state.hrPermission
+        ? 'Permission not granted'
+        : !state.hrDetected
+            ? 'Reading...'
+            : '${state.hrData.round()} bpm\n'
+                'timestamp: ${state.hrTimestamp.round()}\n'
+                'accuracy: ${state.hrAccuracy}';
+
+    final String txt = 'PPG\n$ppgTxt\n\nHeart Rate\n$hrTxt';
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.only(left: MediaQuery.of(context).size.width / 8),
+        child: Text(
+          txt,
+          style: TextStyle(color: Colors.white),
+          textAlign: TextAlign.left,
+        ),
       ),
-      home: ourSensor(),
     );
   }
 }
--- lib/sensor.dart
+++ lib/sensor.dart
@@ -1,12 +1,31 @@
-import 'package:flutter/material.dart';
+import 'package:ppg/ppg.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-class ourSensor extends StatelessWidget {
-  const ourSensor({Key key}) : super(key: key);
+class AppState extends Model {
+  bool ppgDetected = false, hrDetected = false, hrPermission = false;
+  List<double> ppgData;
+  double hrData, ppgTimestamp, hrTimestamp;
+  int hrAccuracy, ppgAccuracy;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        //put code here
-        );
+  initPPG() => ppgEvents.listen(onNewPPGData);
+
+  initHR() {
+    hrPermission = true;
+    hrEvents.listen(onNewHRData);
+    notifyListeners();
+  }
+
+  onNewPPGData(PPGEvent e) {
+    ppgTimestamp = e.t;
+    ppgData = e.x;
+    ppgDetected = true;
+    notifyListeners();
+  }
+
+  onNewHRData(HREvent e) {
+    hrTimestamp = e.t;
+    hrData = e.x;
+    hrDetected = true;
+    notifyListeners();
   }
 }
